361d1dcea8f385cf374ef819507bfc2dd8aeb83f
Joao Mesquita <joaovictorengp@gmail.com>
2020-06-30 23:11:06 -0300

    Standard API response and Middlewares

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,12 +1,35 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
+const { Account } = require('../models');
+
 const router = express.Router();
+
+const saltRounds = 10;
 
 router.get('/signIn', (req, res) => {
   res.json('Sing In');
 });
 
-router.get('/signUp', (req, res) => {
-  res.json('Sing Up');
+router.get('/signUp', async (req, res) => {
+  const { email, password } = req.body;
+
+  const account = await Account.findOne({ where: { email } });
+  if (account) return res.jsonBadRequest(null, 'Account already exists');
+  // const email = 'tst@tst.com';
+  // const password = '123456';
+
+  const hash = bcrypt.hashSync(password, saltRounds);
+
+  const newAccount = await Account.create({
+    email,
+    password: hash,
+  });
+
+  // Account.create({ email: 'tst@tst.com', password: '123456' }).then().catch();
+
+  // res.json('Sing Up');
+  return res.jsonOK(newAccount, 'Account created !');
+  // res.json({ email, password: hash });
 });
 
 module.exports = router;
